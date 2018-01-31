ea022654695572729df1153c771c0d929d8d29e2
Noah Pineda <npineda002@student.butte.edu>
2018-01-31 01:23:14 +0000

    Dogs

---
+++ Dogs/Dogs.cpp
@@ -0,0 +1,32 @@
+/*
+ * Name        : Dogs.cpp
+ * Author      : Noah Pineda
+ * Description : Gets information about dogs from a file then takes input from the user to identify what characteristics they are looking for in a dog then outputs the best matches to a file.
+ This program is intended to make the adoption process easier.
+ * Sources     : File I/O(http://www.cplusplus.com/doc/tutorial/files/)
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+Dog {
+    
+    private:
+}
+
+int main(){
+  //Opens file "Dogs.txt" which contains a list of dogs and creates an object of class Dog for each dog from the list and stores their characteristics in an array of boolean values.(
+  //}
+  //User inputs their desired characteristics stored in boolean values.{
+  //}
+  //Compare User input to the values from the dog characteristic file.{
+  //}
+  //Output a list of dogs based on compatability.{
+  ofstream fout;
+  fout.open ("Compatability.txt");
+  fout << "Writing this to a file.\n";
+  fout.close();
+  //}
+  return 0;
+  
+}
