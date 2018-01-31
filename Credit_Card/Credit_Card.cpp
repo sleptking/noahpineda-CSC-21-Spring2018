30313ed1efc7ed8189c7460afb55002a8c78eb92
Noah Pineda <npineda002@student.butte.edu>
2018-01-31 01:28:26 +0000

    Credit_Card

---
+++ Credit_Card/Credit_Card.cpp
@@ -0,0 +1,15 @@
+/*
+ * Name        : Credit Card.cpp
+ * Author      : Noah Pineda
+ * Description : 
+*/
+#include <iostream>
+#include <fstream)>
+using namespace std;
+
+int main()
+{
+   //Get Input from the credic card file.
+   //Read each credit card number and determine which card it is based on the BIN. If card isnt on the list output "Unknown Card Type"
+   
+}
