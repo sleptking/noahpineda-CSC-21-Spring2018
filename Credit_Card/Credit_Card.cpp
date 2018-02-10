50ca467c378032214dc55817b3ef25f77cc89875
Noah Pineda <npineda002@student.butte.edu>
2018-02-10 00:17:09 +0000

    Credit_Card

--- Credit_Card/Credit_Card.cpp
+++ Credit_Card/Credit_Card.cpp
@@ -11,23 +11,31 @@ using namespace std;
 int Count();
 string CardNumber(int linenumber);
 string CardType(string card);
-bool LuhnCheck();
+bool LuhnCheck(string card);
 
 int main()
 {
    int count = Count();
-   //Looping each card number individually through the BIN check
+   //Looping each card number individually through the BIN and Luhn Algorithm check to determine the validity of the card and the type
    for(int i = 1; i <= count; i++){
-   string number;
-   number = CardNumber(i);
-   string type;
-   type = CardType(number);
-   cout << "type: " << type << " " << "number:" << number << endl;
+      string number;
+      number = CardNumber(i);
+      bool valid = false;
+      valid = LuhnCheck(number);
+      if(valid == true){
+         string type;
+         type = CardType(number);
+         cout << "type: " << type << " " << "number: " << number << endl;
+         }
+      else{
+         cout << "Invalid Card" << endl;
+      }
    }
    return 0;
 }
 
 int Count(){
+   //Funcion counts number of lines in the file
    ifstream fin;
    int count;
    //Open Cards file
@@ -43,6 +51,7 @@ int Count(){
 }
 
 string CardNumber(int linenumber){
+   //Function pulls a card number from the file
    ifstream fin;
    string checknumber;
    //Open Cards file
@@ -61,56 +70,111 @@ string CardNumber(int linenumber){
 }
 
 string CardType(string card){
-   //Read each credit card number and determine which card it is based on the BIN. If card isn't on the list output "Unknown Card Type"
-   card = "378282246310005";
-   string type("none");
-   while(type == "none"){
-      if(card.length() == 15 && card.at(0) == 3){
-         if(card.at(1) == 4 || card.at(1) == 7){
+   //Function reads each credit card number and determines which card it is based on the BIN. If card isn't on the list outputs "Unknown Card Type"
+   stringstream ss;
+   int prefix;
+   string type("Unknown Card Type");
+   ss.clear();
+   ss.str("");
+   ss << card.at(0) << card.at(1);
+   ss >> prefix;
+   //Compares the card number to the lengths and prefixes determined in the BIN.
+        if(card.length() == 15 && (prefix == 34 || prefix == 37)){
                 type = "American Express";
-         }
-      }
-         else if(card.length() == 16){
-            stringstream ss;
+        }
+        else if(card.length() == 16){
             ss << card.at(0) << card.at(1) << card.at(2) << card.at(3) << card.at(4) << card.at(5);
-            int prefix;
+            ss.clear();
+            ss.str("");
             ss >> prefix;
             if(622126 <= prefix && prefix <= 622925){
-               type = "Master Card";
+               type = "Discover";
             }
+            ss.clear();
+            ss.str("");
             ss << card.at(0) << card.at(1) << card.at(2) << card.at(3);
             ss >> prefix;
             if(prefix == 6011){
-               type = "Master Card";
+               type = "Discover";
             }
+            ss.clear();
+            ss.str("");
             ss << card.at(0) << card.at(1) << card.at(2);
             ss >> prefix;
             if(644 <= prefix && prefix <= 649){
-               type = "Master Card";
+               type = "Discover";
             }
+            ss.clear();
+            ss.str("");
             ss << card.at(0) << card.at(1);
             ss >> prefix;
             if(prefix == 65){
-               type = "Master Card";
+               type = "Discover";
             }
+            ss.clear();
+            ss.str("");
             ss << card.at(0) << card.at(1);
             ss >> prefix;
             if(51 <= prefix && prefix <= 55){
-               type = "Discover";
+               type = "MasterCard";
             }
             else if(card.at(0) == 4){
                type = "VISA";
             }
          }
-         else if(13 <= card.length() && card.length() <= 16 && card.at(0) == 4){
+         if(13 <= card.length() && card.length() <= 16){
+            ss.clear();
+            ss.str("");
+            ss << card.at(0);
+            ss >> prefix;
+            if(prefix == 4)
             type = "VISA";
          }
-         else
-            type = "Unknown Card Type";
-    }
+   //Returns the card type
    return type;
 }
 
-bool LuhnCheck(){
-
+bool LuhnCheck(string card){
+   //Function recieves a card number then checks if the card is valid using the Luhn Algorithm
+   stringstream luhnss;
+   stringstream ss;
+   //Flips the card number into a strinstream
+   for(int i = 1;i < card.length();i++){
+         if(i > 0)
+         luhnss << card.at((card.length() - 1) - i) << " ";
+         else
+         luhnss << card.at((card.length() - 1) - i);
+   }
+   //Runs card number through Luhn Algorithm
+   int temp;
+   int other(1), dub(0), sum(0);
+   for(int i = 1;i < card.length();i++){
+        luhnss >> temp;
+        if(other == 1){
+            dub = temp * 2;
+            if(dub <= 9 && dub >= 0)
+                sum = sum + dub;
+            else
+                sum = sum + (dub - 9);
+            other = 2;
+            }
+        else{
+            sum = sum + temp;
+            other = 1;
+        }
+      
+   }
+   //Compares the check number from Luhn Algorithm to the check number from the card
+   bool valid;
+   ss << card.back();
+   int back;
+   int checkdigit;
+   ss >> back;
+   checkdigit = ((sum * 9)%10);
+   if(checkdigit == back)
+      valid = true;
+   else
+      valid = false;
+   //Returns true for valid cards and false for invalid cards
+   return valid;
 }
